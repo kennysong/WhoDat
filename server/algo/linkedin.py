365dfd02e3525c0f75f54e196b9ca8f066928121
Kenny Song <kenny.ysong@gmail.com>
2014-01-18 18:21:56 -0500

    Much better linkedin search scraper

--- server/algo/linkedin.py
+++ server/algo/linkedin.py
@@ -1,23 +1,58 @@
 import requests
 import lxml.html
 from lxml.cssselect import CSSSelector
-
-## todo: Make search more robust by going to each individual profile page
+import re
 
 def get_companies(name):
-	"""Returns list of companies from LinkedIn"""
-	first = name.split()[0]
-	last = name.split()[-1]
-	url = "https://www.linkedin.com/pub/dir/?first=%s&last=%s"%(first,last)
+    """Returns list of companies from LinkedIn"""
+    companies = []
+    first = name.split()[0]
+    last = name.split()[-1]
+    url = "https://www.linkedin.com/pub/dir/?first=%s&last=%s"%(first,last)
 
-	html = requests.get(url).text
-	tree = lxml.html.fromstring(html)
-	sel = CSSSelector('.vcard-basic dd, .title')
+    # Get page from searching for first last name
+    html = requests.get(url).text
+    tree = lxml.html.fromstring(html)
+    sel_a = CSSSelector('h2 strong a')
 
-	search = sel(tree)
-	jobs = [result.text for result in search]
+    # Check if page is empty
+    sel_null = CSSSelector('.null-results')
+    if (not sel_null(tree)):
+        return companies
 
-	companies = [job.split(' at ')[-1].lower() for job in jobs]
+    # Determine if page is search results or profile page (ie. only one result)
+    sel_title = CSSSelector('title')
+    if ('profiles' in sel_title(tree)[0].text):
+        # If page shows results
+        
+        # Get profile links on results page
+        search = sel_a(tree)
+        links = [result.get('href') for result in search]
 
-	return companies
-	
+
+        # Go to each profile page of results
+        for link in links:
+            print 'Getting: ' + link
+            html = requests.get(link).text
+
+            sel_job = CSSSelector('.headline-title, .title')
+            search = sel_job(tree)
+            job = search[0].text
+            company = headline.split(' at ')[-1]
+            companies.append(company) 
+            
+        
+    else:
+        # If page shows a profile
+        sel_job = CSSSelector('.headline-title, .title')
+        search = sel_job(tree)
+        job = search[0].text
+        company = headline.split(' at ')[-1]
+        companies.append(company) 
+
+    return companies
+        
+            
+            
+    
+    
