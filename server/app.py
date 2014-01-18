e05bb7d32a3c3caf6410459679951f010278c808
Jared Zoneraich <jszoneraich@gmail.com>
2014-01-18 14:21:55 -0500

    email validator

--- server/app.py
+++ server/app.py
@@ -3,6 +3,8 @@ import jinja2
 from flask import Flask, render_template, redirect, request, jsonify
 from flask.ext.pymongo import PyMongo
 from validate_email import validate_email
+import smtplib
+from flask import current_app
 # from flanker.addresslib import address
 
 app = Flask(__name__)
@@ -17,6 +19,48 @@ def is_valid(email):
 	# return True
 	return validate_email(email,check_mx=True,verify=True)
 
+def is_valid_manual(email):
+	"""Returns:
+		-1 --- Cannot verify
+		0  --- Invalid Email
+		1  --- Valid Email
+	"""
+
+	maildomain = email.split("@")[1]
+	nsToken = "mail exchanger = "
+	mailservers = {}
+	plines = os.popen("nslookup -type=MX "+maildomain).readlines()
+	for pline in plines:
+		if nsToken in pline:
+			x = (pline.split(nsToken)[1].strip()).split(' ')
+			number = None
+			try:
+				number = int(x[0])
+			except:
+				continue
+			x.remove(x[0])
+			x = "".join(x)
+			mailservers[number] = x
+
+	if len(mailservers.keys()) == 0:
+		return 1
+	minimum = min(mailservers.keys())
+	mailserver = mailservers[minimum]
+
+	s = smtplib.SMTP(mailserver)
+	rep1= s.ehlo()
+	if rep1[0]==250 : #250 denotes OK reply
+		rep2=s.mail("test@rediff.com")
+		if rep2[0] == 250:
+			rep3 = s.rcpt(email)
+			if rep3[0] == 250:
+				if validate_email(email,check_mx=True,verify=True):
+					return 0
+				else:
+					return 1
+			elif rep3[0] == 550: #email invalid
+				return 1
+	return .5
 
 @app.route('/', methods=['GET','POST'])
 def home_page():
@@ -29,8 +73,10 @@ def home_page():
 		name = request.form['name']
 		# validate_email(email,check_mx=True,verify=True)
 		# https://github.com/mailgun/flanker
-		return is_valid(name.replace(' ','.')+"@gmail.com")
-		return jsonify(email=name.replace(' ','.')+"@gmail.com")
+		valid = is_valid_manual(name.replace(' ','.') + "@gmail.com")
+		message = {-1 : "Unable to verify email", 0 : "Invalid email", 1 : "Valid Email"}
+		# return message[valid]
+		return jsonify(email=name.replace(' ','.')+"@gmail.com",message=message[valid])
 	else:
 		return "Who Dat? - GET"
 
